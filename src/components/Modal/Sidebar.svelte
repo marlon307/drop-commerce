4571678e3f8b338892fb6151787f62e7cb8352b6
marlon307 <marlon-ramosb@hotmail.com>
2023-10-06 12:19:25 -0300

    ADDC: Sidebar cart

---
+++ src/components/Modal/Sidebar.svelte
@@ -0,0 +1,135 @@
+<script>
+  import { fly, fade } from "svelte/transition";
+  export let show = false;
+</script>
+
+{#if show}
+  <div
+    class="inset-0 fixed bg-black/30 backdrop-blur-sm z-50"
+    transition:fade={{ duration: 100 }}
+  />
+  <section
+    class="fixed w-full bg-neutral-950 border-l border-neutral-700 h-full top-0 right-0 max-w-md p-6 z-50 flex flex-col"
+    transition:fly={{ x: 448, opacity: 1 }}
+  >
+    <div class="flex justify-between items-center mb-4">
+      <h1 class="text-neutral-100 text-xl font-semibold">Carrinho</h1>
+      <button
+        class="p-2 rounded-lg border border-neutral-700 float-right"
+        on:click={() => (show = !show)}
+        data-close
+      >
+        <svg
+          width="15"
+          height="15"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="text-white w-6 h-6"
+          ><path
+            d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+            fill="currentColor"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
+    <ul class="flex-1 block overflow-auto">
+      <li class="flex gap-4 border-b border-neutral-700 py-6">
+        <a href="/" class="flex gap-4 justify-between">
+          <figure
+            class="w-20 h-20 rounded-lg border border-neutral-700 hover:bg-neutral-800 bg-neutral-900 overflow-hidden"
+          >
+            <img
+              src="https://images.yampi.me/assets/stores/3072/uploads/images/cabo-carredaor-iphone-651f3697833b0-medium.png"
+              alt="sdasdas"
+              loading="lazy"
+              class="object-fill h-full w-full"
+            />
+          </figure>
+          <div class="flex-1">
+            <span class="text-neutral-100 text-xl text-left line-clamp-2">
+              Cabo carredaor iPhone
+            </span>
+            <div class="text-neutral-400 font-light">Brnaco / G</div>
+          </div>
+        </a>
+        <div class="text-neutral-100 flex flex-col gap-5 items-start">
+          <span>
+            {(32).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </span>
+          <div
+            class="ml-auto flex h-9 flex-row items-center rounded-full border border-neutral-200 dark:border-neutral-700"
+          >
+            <button
+              aria-label="Reduzir quantidade do item"
+              class="ease flex h-full min-w-[36px] max-w-[36px] flex-none items-center justify-center rounded-full px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80 ml-auto"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+                class="h-4 w-4 dark:text-neutral-500"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 12h-15"
+                /></svg
+              ></button
+            >
+            <p class="w-6 text-center"><span class="w-full text-sm">1</span></p>
+            <button
+              aria-label="Adicionar quantidade do item"
+              class="ease flex h-full min-w-[36px] max-w-[36px] flex-none items-center justify-center rounded-full px-2 transition-all duration-200 hover:border-neutral-800 hover:opacity-80"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+                class="h-4 w-4 dark:text-neutral-500"
+                ><path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <div class="w-full py-4">
+      <div
+        class="py-2 border-b border-neutral-700 flex justify-between items-center mb-3"
+      >
+        <span class="text-neutral-400">Entrega</span>
+        <span class="text-neutral-400">Calculado no checkout</span>
+      </div>
+      <div
+        class="py-2 border-b border-neutral-700 flex justify-between items-center mb-3"
+      >
+        <span class="text-neutral-400">Total</span>
+        <span class="text-neutral-100 text-xl"
+          >{(32).toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </span>
+      </div>
+    </div>
+    <a
+      href="/"
+      class="py-3 bg-orange-500 rounded-full text-center text-orange-100 font-medium opacity-90 hover:opacity-100"
+      >Finalizar Compra</a
+    >
+  </section>
+{/if}
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,6 +3,7 @@
 	import Search from "../components/Search/index.svelte";
 	import logo from "$lib/images/svelte-logo.svg";
 	import User from "../components/Menu/User.svelte";
+	import Cart from "../components/Menu/Cart.svelte";
 </script>
 
 <header class="py-4 bg-neutral-900 flex items-center justify-between gap-4">
@@ -56,25 +57,7 @@
 				<User />
 			</li>
 			<li>
-				<button
-					class="relative flex h-11 w-11 items-center justify-center rounded-md border transition-colors border-neutral-700 text-white"
-					aria-label="Carrinho"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
-						class="h-4 transition-all ease-in-out hover:scale-110"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-						/>
-					</svg>
-				</button>
+				<Cart />
 			</li>
 		</ul>
 	</nav>
