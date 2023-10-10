c8c577d2f7f007ca8426f3005de63f0c4297c156
marlon307 <marlon-ramosb@hotmail.com>
2023-10-10 07:10:05 -0300

    FEAT: add dotloading

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,22 @@ export default {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' }
         },
+        dotbounce:{
+          '0%, 100%': {
+            transform: 'translateY(-25%)',
+            'animation-timing-function': 'cubic-bezier(0.8,0,1,1)',
+            opacity: 0.4,
+          },
+          '20%': {
+            transform: 'none',
+            'animation-timing-function': 'cubic-bezier(0,0,0.2,1)',
+            opacity: 1,
+          },
+        }
       },
       animation: {
         carousel: 'marquee 60s linear infinite',
+        bouncedot: 'dotbounce 1s infinite',
       }
     },
   },
