9294df2cc4191dcfdcfe411f2748ff686d70fa5f
marlon307 <marlon-ramosb@hotmail.com>
2023-10-10 17:21:43 -0300

    UP: Add button remove cart

--- src/components/Menu/index.ts
+++ src/components/Menu/index.ts
@@ -1,4 +1,4 @@
-import Cart from "./Cart.svelte";
+import Cart from "./Cart/index.svelte";
 import CategoriesSearch from "./CategoriesSearch.svelte";
 import User from "./User.svelte";
 export { Cart, CategoriesSearch, User };
