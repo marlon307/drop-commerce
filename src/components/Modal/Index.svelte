4718798699fd9ed2793cc1a3a3fac6bd5361ca17
marlon307 <marlon-ramosb@hotmail.com>
2023-10-19 18:00:59 -0300

    UP: show modal detail order

--- src/components/Modal/Index.svelte
+++ src/components/Modal/Index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   export let showModal: boolean; // boolean
+  export let title: string;
+
   let dialog: HTMLDialogElement; // HTMLDialogElement
 
-  $: if (dialog && showModal) dialog.show();
+  $: if (dialog && showModal) dialog.showModal();
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
@@ -10,31 +12,34 @@
   bind:this={dialog}
   on:close={() => (showModal = false)}
   on:click|self={() => dialog.close()}
-  class="bg-neutral-950 w-full m-0 appearance-none h-full top-0 z-50 p-6 fixed"
+  class="bg-neutral-950 w-full max-w-xl appearance-none top-0 z-50 fixed rounded-2xl border border-neutral-800 backdrop:bg-black/70 backdrop:backdrop-blur-sm"
 >
   <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <div on:click|stopPropagation class="block">
-    <slot />
-    <button
-      on:click={() => dialog.close()}
-      type="button"
-      class="p-2 rounded-lg border border-neutral-700 bottom-6 left-1/2 -translate-x-1/2 absolute"
-    >
-      <svg
-        width="15"
-        height="15"
-        viewBox="0 0 15 15"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        class="text-white w-6 h-6 transition-all ease-in-out hover:scale-110"
+  <div on:click|stopPropagation class="block p-6 h-full">
+    <div class="flex items-center justify-between w-full mb-6">
+      <h1 class="text-neutral-100 font-medium text-2xl">{title}</h1>
+      <button
+        on:click={() => dialog.close()}
+        type="button"
+        class="p-2 rounded-lg border border-neutral-700"
       >
-        <path
-          d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
-          fill="currentColor"
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </button>
+        <svg
+          width="15"
+          height="15"
+          viewBox="0 0 15 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="text-white w-6 h-6 transition-all ease-in-out hover:scale-110"
+        >
+          <path
+            d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+            fill="currentColor"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
+    <slot />
   </div>
 </dialog>
--- src/routes/user/orders/+page.svelte
+++ src/routes/user/orders/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+  import Modal from "../../../components/Modal/Index.svelte";
   export let data;
+
+  let showModal = false;
+  let orderId: IOrder = data.orders![0];
 </script>
 
 <table class="mb-4 w-full text-sm">
@@ -23,9 +27,13 @@
     </tr>
   </thead>
   <tbody class="block max-h-screen w-full overflow-auto rounded-xl">
-    {#each data.orders as order}
+    {#each data?.orders || [] as order}
       <tr
         class="table h-16 w-full table-fixed cursor-pointer border-b border-neutral-700 bg-neutral-900 text-neutral-300 last:border-none"
+        on:click={() => {
+          orderId = order;
+          showModal = true;
+        }}
       >
         <td class="w-auto md:w-3/6 px-4 py-2">
           <p>{order.name}</p>
@@ -57,3 +65,117 @@
     {/each}
   </tbody>
 </table>
+
+<Modal bind:showModal title={`Pedido - ${orderId?.name}`}>
+  {#if orderId?.financialStatus}
+    <span class="text-neutral-100 text-lg">
+      Status: {orderId.financialStatus}
+    </span>
+    <ul class="w-full mb-6 overflow-auto block max-h-[35vh]">
+      {#each orderId.lineItems as product}
+        <li
+          class="flex gap-4 border-b border-neutral-700 py-6 relative justify-between"
+        >
+          <a
+            href={`/product/${product.variant.product.handle}`}
+            class="flex gap-4 justify-between"
+          >
+            <figure
+              class="overflow-hidden w-20 h-20 rounded-lg border border-neutral-700 bg-neutral-900"
+            >
+              <img
+                src={product.variant.image.transformedSrc}
+                alt={product.variant.product.title}
+                loading="lazy"
+                class="object-fill h-full w-full"
+                width="80"
+                height="80"
+              />
+            </figure>
+            <div class="flex-1 flex flex-col justify-between">
+              <span class="text-neutral-100 text-xl text-left line-clamp-2">
+                {product.variant.product.title}
+              </span>
+              <div
+                class="flex gap-1 text-neutral-500 items-center justify-start"
+              >
+                <span class="font-light" title="Title - Default Title">
+                  {product.variant.title}
+                </span>
+              </div>
+            </div>
+          </a>
+          <div class="text-neutral-100 flex flex-col gap-5 items-start">
+            <span>
+              {Number(product.variant.price.amount).toLocaleString("BRL", {
+                currency: product.variant.price.currencyCode,
+                style: "currency",
+              })}
+            </span>
+            <div
+              class="ml-auto flex py-1 px-2 flex-row items-center rounded-full border border-neutral-200 dark:border-neutral-700"
+            >
+              <p class="w-6 text-center">
+                <span class="w-full text-sm">{product.quantity}</span>
+              </p>
+            </div>
+          </div>
+        </li>
+      {/each}
+    </ul>
+    <div class="block mb-6 text-neutral-100">
+      <dl class="flex gap-1">
+        <dt class="text-neutral-400 font-light">Entregar para:</dt>
+        <dd>
+          {`${orderId.shippingAddress.firstName} ${orderId?.shippingAddress.lastName}`}
+        </dd>
+      </dl>
+      <dl class="flex gap-1 flex-wrap">
+        <dt class="text-neutral-400 font-light">Endereço:</dt>
+        <dd>{orderId.shippingAddress.address1},</dd>
+        <dd>{orderId.shippingAddress.address2},</dd>
+        <dd>{orderId.shippingAddress.zip},</dd>
+        <dd>{orderId.shippingAddress.city},</dd>
+        <dd>{orderId.shippingAddress.provinceCode},</dd>
+        <dd>{orderId.shippingAddress.country}</dd>
+      </dl>
+      <dl class="flex gap-1 flex-wrap">
+        <dt class="text-neutral-400 font-light">Transportadora:</dt>
+        <dd>{orderId.shippingAddress.company}</dd>
+      </dl>
+    </div>
+    <div class="w-full flex-1">
+      <div
+        class="py-2 border-b border-neutral-700 flex justify-between items-center mb-3"
+      >
+        <span class="text-neutral-400">Taxas</span>
+        <span class="text-neutral-100 text-xl">
+          {Number(orderId.totalTax.amount).toLocaleString("BRL", {
+            currency: orderId.totalTax.currencyCode,
+            style: "currency",
+          })}
+        </span>
+      </div>
+      <div
+        class="py-2 border-b border-neutral-700 flex justify-between items-center mb-3"
+      >
+        <span class="text-neutral-400">Entrega</span>
+        <span class="text-neutral-400">
+          {Number(orderId.totalShippingPrice.amount).toLocaleString("BRL", {
+            currency: orderId.totalShippingPrice.currencyCode,
+            style: "currency",
+          })}
+        </span>
+      </div>
+      <div class="py-2 flex justify-between items-center mb-3">
+        <span class="text-neutral-400">Total</span>
+        <span class="text-neutral-100 text-xl">
+          {Number(orderId.totalPrice.amount).toLocaleString("BRL", {
+            currency: orderId.totalPrice.currencyCode,
+            style: "currency",
+          })}
+        </span>
+      </div>
+    </div>
+  {/if}
+</Modal>
