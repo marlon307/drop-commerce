b903e8643262f8989f96c21c3a71cab265b2c158
marlon307 <marlon-ramosb@hotmail.com>
2023-10-29 16:04:45 -0300

    UP: refactore transformObject

--- src/lib/transformObject.ts
+++ src/lib/transformObject.ts
@@ -1,40 +1,31 @@
 // https://gist.github.com/ManUtopiK/469aec75b655d6a4d912aeb3b75af3c9
 
-type JsonArray = Json[];
-type JsonValue = string | number | boolean | null;
-type JsonObject = { [key: string]: Json };
-
-export type Json =
-  | JsonArray
-  | JsonValue
-  | JsonObject;
+type Json =
+  | Json[]
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  ;
 
 type JsonObj = { [key: string]: Json };
 
-function isObject(input: Json): input is JsonObject {
+function isObject(input: Json): input is JsonObj {
   return typeof input === "object" && input !== null && !Array.isArray(input);
 }
 
 export default function transformObject(obj: Json): Json {
   if (isObject(obj)) {
-    if (Array.isArray(obj) && obj.every(isObject)) {
-      return obj.map((element) => transformObject(element));
-    } else if (obj.edges && Array.isArray(obj.edges)) {
-      return obj.edges.map((edge) => {
-        if (edge && isObject(edge)) {
-          return transformObject(edge.node);
-        }
-        return edge;
-      });
-    }
+    if (obj.edges && Array.isArray(obj.edges))
+      return obj.edges.map((edge) => edge && isObject(edge) ? transformObject(edge.node) : edge);
+
     return Object.keys(obj).reduce((result: JsonObj, key) => {
       const value = obj[key];
-      result[key] = isObject(value) ? transformObject(value) : value;
+      result[key] = isObject(value) ? transformObject(value) : obj[key];
       return result;
     }, {} as JsonObj);
   }
-  if (Array.isArray(obj)) {
-    return obj.map(transformObject);
-  }
+  if (Array.isArray(obj)) return obj.map(transformObject);
   return obj;
 }
