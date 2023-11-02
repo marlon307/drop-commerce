e3486123805cd615223fd9c168f9a6d0c74b61d2
marlon307 <marlon-ramosb@hotmail.com>
2023-11-02 08:43:30 -0300

    TEST: cache fetch shopify

--- src/lib/shopify/index.ts
+++ src/lib/shopify/index.ts
@@ -8,7 +8,13 @@ import { queryCustomer, queryCustomerAddress, queryCustomerOrders } from './quer
 import { getProductByHandler, getProductsCollectionQuery, getProductsQuery } from './query/product';
 import { predictiveSearchQuery } from "./query/search";
 
-async function fetchShopify(query: string, variables?: object): Promise<any> {
+interface IFetchShopify {
+  query: string;
+  variables?: object;
+  cache?: RequestCache;
+}
+
+async function fetchShopify({ query, variables, cache }: IFetchShopify): Promise<any> {
   try {
     const data = await fetch(SHOPIFY_API_END_POINT, {
       method: 'POST',
@@ -16,10 +22,11 @@ async function fetchShopify(query: string, variables?: object): Promise<any> {
         'Content-Type': 'application/json',
         'X-Shopify-Storefront-Access-Token': SHOPIFY_ACCESS_TOKEN,
       },
+      cache,
       body: JSON.stringify({
         ...(query && { query }),
         ...(variables && { variables })
-      })
+      }),
     });
     return transformObject(await data.json());
   } catch (error) {
@@ -30,86 +37,150 @@ async function fetchShopify(query: string, variables?: object): Promise<any> {
 }
 
 export async function getProductsCollection(collection: string, sort: string = 'RELEVANCE', reverse: boolean = false) {
-  const res = await fetchShopify(getProductsCollectionQuery, { collection, sort, reverse });
+  const res = await fetchShopify({
+    query: getProductsCollectionQuery,
+    variables: {
+      collection,
+      sort,
+      reverse,
+    }
+  });
   return res.data?.collectionByHandle.products;
 }
 
 export async function getProducts(query: string, sort: string = 'RELEVANCE', reverse: boolean = false) {
-  const res = await fetchShopify(getProductsQuery, { query, sort, reverse });
+  const res = await fetchShopify({
+    query: getProductsQuery,
+    variables: { query, sort, reverse }
+  });
   return res.data.products;
 }
 
 export async function getProductByHandle(handle: string) {
-  const res = await fetchShopify(getProductByHandler, { handle });
+  const res = await fetchShopify({
+    query: getProductByHandler,
+    variables: { handle }
+  });
   return res.data.productByHandle;
 }
 
 export async function createCart(linesItems: ILinesCart[]) {
-  const res = await fetchShopify(createCartShopify, { linesItems });
+  const res = await fetchShopify({
+    query: createCartShopify,
+    variables: { linesItems },
+    cache: 'no-store'
+  });
   return res.data.cartCreate.cart;
 }
 
 export async function updateCartItem(cartId: string, linesItems: ILinesCart) {
-  const res = await fetchShopify(updateCartShopify, { cartId, linesItems });
+  const res = await fetchShopify({
+    query: updateCartShopify,
+    variables: { cartId, linesItems },
+    cache: 'no-store'
+  });
   return res.data.cartLinesUpdate.cart;
 }
 
 export async function removeCartItem(cartId: string, lineIds: string[]) {
-  const res = await fetchShopify(removeCartShopify, { cartId, lineIds });
+  const res = await fetchShopify({
+    query: removeCartShopify,
+    variables: { cartId, lineIds },
+    cache: 'no-store'
+  });
   return res.data.cartLinesRemove.cart;
 }
 
 export async function addCartItem(cartId: string, linesItems: ILinesCart[]) {
-  const res = await fetchShopify(addCartShopify, { cartId, linesItems });
+  const res = await fetchShopify({
+    query: addCartShopify,
+    variables: { cartId, linesItems },
+    cache: 'no-store'
+  });
   return res.data.cartLinesAdd.cart;
 }
 
 export async function getCartId(idCart: string) {
-  const res = await fetchShopify(getCartIdMutation, { idCart });
+  const res = await fetchShopify({
+    query: getCartIdMutation,
+    variables: { idCart },
+    cache: 'no-store'
+  });
   return res.data?.cart || { cart: {} };
 }
 
 export async function registerCustomer(input: object) {
-  const res = await fetchShopify(createCustomer, { input });
+  const res = await fetchShopify({
+    query: createCustomer,
+    variables: { input },
+    cache: 'no-store'
+  });
   return res.data?.customerCreate || { customer: {} };
 }
 
 export async function accessTokenCustomerCreate(input: object) {
-  const res = await fetchShopify(customerAccessTokenCreate, { input });
+  const res = await fetchShopify({
+    query: customerAccessTokenCreate,
+    variables: { input },
+    cache: 'no-store'
+  });
   return res.data?.customerAccessTokenCreate || { customerAccessToken: {} };
 }
 
 export async function updateCustomer(token: string, customer: object) {
-  const res = await fetchShopify(customerUpdate, { token, customer });
+  const res = await fetchShopify({
+    query: customerUpdate,
+    variables: { token, customer }
+  });
   return res.data?.customerUpdate || { updateCustomer: {} };
 }
 
 export async function getCustomerOrders(token: string) {
-  const res = await fetchShopify(queryCustomerOrders, { token });
+  const res = await fetchShopify({
+    query: queryCustomerOrders,
+    variables: { token }
+  });
   return res.data?.customer;
 }
 
 export async function getCustomerAccessToken(token: string) {
-  const res = await fetchShopify(queryCustomer, { token });
+  const res = await fetchShopify({
+    query: queryCustomer,
+    variables: { token }
+  });
   return res.data;
 }
 
 export async function getCustomerAddress(token: string) {
-  const res = await fetchShopify(queryCustomerAddress, { token });
+  const res = await fetchShopify({
+    query: queryCustomerAddress,
+    variables: { token }
+  });
   return res.data?.customer;
 }
 
 export async function updateCustomerAddress(token: string, idAddress: string, dataAddress: IAddress) {
-  const res = await fetchShopify(customerAddressUpdate, { token, idAddress, dataAddress });
+  const res = await fetchShopify({
+    query: customerAddressUpdate,
+    variables: { token, idAddress, dataAddress },
+    cache: 'no-store'
+  });
   return res.data?.customerAddressUpdate;
 }
 
 export async function deleteCustomerAddress(token: string, idAddress: string) {
-  const res = await fetchShopify(customerAddressDelete, { token, idAddress });
+  const res = await fetchShopify({
+    query: customerAddressDelete,
+    variables: { token, idAddress }
+
+  });
   return res.data?.customerAddressDelete;
 }
 
 export async function predictiveSearchProducts(query: string) {
-  const res = await fetchShopify(predictiveSearchQuery, { query });
+  const res = await fetchShopify({
+    query: predictiveSearchQuery,
+    variables: { query }
+  });
   return res.data?.predictiveSearch.products;
 }
