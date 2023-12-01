ea329af355641738c1a9b679493a893b63ab35e6
marlon307 <marlon-ramosb@hotmail.com>
2023-11-30 22:20:48 -0300

    UP: add page active account and sort query params pg produtos

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,5 @@
 export const sorting = [
+  { title: 'Relevância', slug: 'relevancia', sortKey: 'RELEVANCE', reverse: false }, // asc
   { title: 'Tendências', slug: 'tendencias', sortKey: 'BEST_SELLING', reverse: false }, // asc
   { title: 'Lançamentos', slug: 'lancamentos', sortKey: 'CREATED_AT', reverse: true },
   { title: 'Menor Preco', slug: 'menor-preco', sortKey: 'PRICE', reverse: false }, // asc
