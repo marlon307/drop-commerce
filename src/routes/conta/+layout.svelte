91e8d1dbf92dadc11c9e976730cf4e3bc419f5bc
marlon307 <marlon-ramosb@hotmail.com>
2023-12-16 13:49:04 -0300

    UP: space mn account

--- src/routes/conta/+layout.svelte
+++ src/routes/conta/+layout.svelte
@@ -18,7 +18,7 @@
       class="rounded-lg text-neutral-400 hover:text-neutral-200 data-[active=true]:bg-neutral-400/20 data-[active=true]:text-neutral-100"
       data-active={$page.url.pathname === "/conta"}
     >
-      <a href="/conta" class="flex items-center gap-2 px-3 py-2">
+      <a href="/conta" class="flex items-center gap-3 px-3 py-2">
         <svg
           width="15"
           height="15"
@@ -40,7 +40,7 @@
       class="rounded-lg text-neutral-400 hover:text-neutral-200 data-[active=true]:bg-neutral-400/20 data-[active=true]:text-neutral-100"
       data-active={$page.url.pathname === "/conta/pedidos"}
     >
-      <a href="/conta/pedidos" class="flex items-center gap-2 px-3 py-2">
+      <a href="/conta/pedidos" class="flex items-center gap-3 px-3 py-2">
         <svg
           width="15"
           height="15"
@@ -62,7 +62,7 @@
       class="rounded-lg text-neutral-400 hover:text-neutral-200 data-[active=true]:bg-neutral-400/20 data-[active=true]:text-neutral-100"
       data-active={$page.url.pathname === "/conta/endereco"}
     >
-      <a href="/conta/endereco" class="flex items-center gap-2 px-3 py-2">
+      <a href="/conta/endereco" class="flex items-center gap-3 px-3 py-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
