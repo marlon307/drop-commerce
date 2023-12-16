9a50662435809633b99d2409c1caac4b64c230ec
marlon307 <marlon-ramosb@hotmail.com>
2023-12-16 07:47:51 -0300

    UP: ADD icons menu

--- src/components/Menu/User.svelte
+++ src/components/Menu/User.svelte
@@ -28,47 +28,104 @@
 </button>
 
 <Dialog bind:showModal>
-  <ul
-    class="mx-auto mt-12 flex flex-col items-center justify-center space-y-2 text-xl"
-  >
+  <ul class="mx-auto mt-12 space-y-3">
     {#if customer?.email}
-      <li>
+      <li class="rounded-lg hover:bg-neutral-400/20">
         <a
           href="/conta"
-          class="text-neutral-100 underline-offset-4 hover:underline"
+          class="flex items-center gap-2 px-3 py-2 text-neutral-100"
         >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            class="h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              d="M7.5 0.875C5.49797 0.875 3.875 2.49797 3.875 4.5C3.875 6.15288 4.98124 7.54738 6.49373 7.98351C5.2997 8.12901 4.27557 8.55134 3.50407 9.31167C2.52216 10.2794 2.02502 11.72 2.02502 13.5999C2.02502 13.8623 2.23769 14.0749 2.50002 14.0749C2.76236 14.0749 2.97502 13.8623 2.97502 13.5999C2.97502 11.8799 3.42786 10.7206 4.17091 9.9883C4.91536 9.25463 6.02674 8.87499 7.49995 8.87499C8.97317 8.87499 10.0846 9.25463 10.8291 9.98831C11.5721 10.7206 12.025 11.8799 12.025 13.5999C12.025 13.8623 12.2376 14.0749 12.5 14.0749C12.7623 14.075 12.975 13.8623 12.975 13.6C12.975 11.72 12.4778 10.2794 11.4959 9.31166C10.7244 8.55135 9.70025 8.12903 8.50625 7.98352C10.0187 7.5474 11.125 6.15289 11.125 4.5C11.125 2.49797 9.50203 0.875 7.5 0.875ZM4.825 4.5C4.825 3.02264 6.02264 1.825 7.5 1.825C8.97736 1.825 10.175 3.02264 10.175 4.5C10.175 5.97736 8.97736 7.175 7.5 7.175C6.02264 7.175 4.825 5.97736 4.825 4.5Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+            </path>
+          </svg>
           Conta
         </a>
       </li>
-      <li>
+      <li class="rounded-lg hover:bg-neutral-400/20">
         <a
           href="/conta/pedidos"
-          class="text-neutral-100 underline-offset-4 hover:underline"
+          class="flex items-center gap-2 px-3 py-2 text-neutral-100"
         >
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4"
+            ><path
+              d="M7.28856 0.796908C7.42258 0.734364 7.57742 0.734364 7.71144 0.796908L13.7114 3.59691C13.8875 3.67906 14 3.85574 14 4.05V10.95C14 11.1443 13.8875 11.3209 13.7114 11.4031L7.71144 14.2031C7.57742 14.2656 7.42258 14.2656 7.28856 14.2031L1.28856 11.4031C1.11252 11.3209 1 11.1443 1 10.95V4.05C1 3.85574 1.11252 3.67906 1.28856 3.59691L7.28856 0.796908ZM2 4.80578L7 6.93078V12.9649L2 10.6316V4.80578ZM8 12.9649L13 10.6316V4.80578L8 6.93078V12.9649ZM7.5 6.05672L12.2719 4.02866L7.5 1.80176L2.72809 4.02866L7.5 6.05672Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            >
+            </path>
+          </svg>
           Pedidos
         </a>
       </li>
-      <li>
+      <li class="rounded-lg hover:bg-neutral-400/20">
         <a
           href="/conta/endereco"
-          class="text-neutral-100 underline-offset-4 hover:underline"
+          class="flex items-center gap-2 px-3 py-2 text-neutral-100"
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="h-4 w-4"
+          >
+            <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path>
+            <circle cx="12" cy="10" r="3"></circle>
+          </svg>
           Endereços
         </a>
       </li>
     {:else}
-      <li>
+      <li class="rounded-lg text-xl hover:bg-neutral-400/20">
         <a
           href="/auth/login"
-          class="text-neutral-100 underline-offset-4 hover:underline"
+          class="flex items-center justify-center gap-2 px-3 py-2 text-neutral-100"
         >
-          Login
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-4 w-4"
+            ><path
+              d="M4.5 1C4.22386 1 4 1.22386 4 1.5C4 1.77614 4.22386 2 4.5 2H12V13H4.5C4.22386 13 4 13.2239 4 13.5C4 13.7761 4.22386 14 4.5 14H12C12.5523 14 13 13.5523 13 13V2C13 1.44772 12.5523 1 12 1H4.5ZM6.60355 4.89645C6.40829 4.70118 6.09171 4.70118 5.89645 4.89645C5.70118 5.09171 5.70118 5.40829 5.89645 5.60355L7.29289 7H0.5C0.223858 7 0 7.22386 0 7.5C0 7.77614 0.223858 8 0.5 8H7.29289L5.89645 9.39645C5.70118 9.59171 5.70118 9.90829 5.89645 10.1036C6.09171 10.2988 6.40829 10.2988 6.60355 10.1036L8.85355 7.85355C9.04882 7.65829 9.04882 7.34171 8.85355 7.14645L6.60355 4.89645Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path></svg
+          >
+          Entrar
         </a>
       </li>
-      <li>
+      <li class="block pt-6">
         <a
           href="/auth/cadastrar"
-          class="text-neutral-100 underline-offset-4 hover:underline"
+          class="block w-full text-center text-base text-neutral-100 underline-offset-4 hover:underline"
         >
           Registrar
         </a>
