a62244697adc48487dc8c0bf20e2b20bea0e1daf
marlon307 <marlon-ramosb@hotmail.com>
2023-12-25 20:10:07 -0300

    UP: enable scrol carrousel in samal scream

--- src/components/Carrousel/index.svelte
+++ src/components/Carrousel/index.svelte
@@ -4,7 +4,7 @@
   export let arrayContent: IPoductCard[];
 </script>
 
-<div class="mt-8 w-full overflow-x-hidden pb-6">
+<div class="mt-8 w-full overflow-auto pb-6 md:overflow-x-hidden">
   <ul class="flex animate-carousel gap-4">
     {#each [...arrayContent, ...arrayContent, ...arrayContent] as content, index ((content.handle, index))}
       <li class="aspect-square w-full max-w-xs flex-none">
