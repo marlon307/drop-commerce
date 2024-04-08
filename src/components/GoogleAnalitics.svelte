74f04bb82d264bdba721ed08713441f8c821da63
marlon307 <marlon-ramosb@hotmail.com>
2024-04-07 22:57:17 -0300

    Google Analitics update

--- src/components/GoogleAnalitics.svelte
+++ src/components/GoogleAnalitics.svelte
@@ -1,22 +1,17 @@
-<script>
-  // data-nscript="lazyOnload"
-  // @ts-ignore
-  import { onMount } from "svelte";
-  onMount(() => {
-    // @ts-ignore
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      // @ts-ignore
-      dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-
-    gtag("config", "G-T13EQM11L4");
-  });
-</script>
-
-<!-- Google tag (gtag.js) -->
 <svelte:head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-T13EQM11L4">
+  <!-- Google Tag Manager -->
+  <!-- https://stackoverflow.com/questions/70952491/how-to-add-google-analytics-tracking-id-in-svelte -->
+  <script>
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+      var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s),
+        dl = l != "dataLayer" ? "&l=" + l : "";
+      j.async = true;
+      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+      f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "G-T13EQM11L4");
   </script>
+  <!-- End Google Tag Manager -->
 </svelte:head>
