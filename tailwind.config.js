baa7cee2a1dd5d5aae66b135716442bd5a236370
marlon307 <marlon-ramosb@hotmail.com>
2024-08-08 19:52:18 -0300

    Add category and banners

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
       keyframes: {
         marquee: {
           'from': { transform: 'translateX(0)' },
-          'to': { transform: 'translateX(-100%)' }
+          'to': { transform: 'translateX(-100vw)' }
         },
         dotbounce: {
           '0%, 100%': {
