2600f3892d233a08c7ec28c73e6fd4673814cb01
marlon307 <marlon-ramosb@hotmail.com>
2025-02-16 17:26:40 -0300

    Prerender home

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,12 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { getProductsCollection } from '$lib/shopify';
 
+export const config = {
+  runtime: "edge"
+};
+
+export const prerender = true;
+
 export const load: PageServerLoad = async () => {
   const productshome = await getProductsCollection('hidden-home-page', 'MANUAL');
   if (productshome) return productshome;
