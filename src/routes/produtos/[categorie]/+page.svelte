9b7c19847a7c4a473e2ab4354c7c8aae5c9ab08d
marlon307 <marlon-ramosb@hotmail.com>
2025-07-19 13:02:52 -0300

    fix handle products

--- src/routes/produtos/[categorie]/+page.svelte
+++ src/routes/produtos/[categorie]/+page.svelte
@@ -4,18 +4,18 @@
 </script>
 
 <svelte:head>
-  <meta name="description" content={data.description} />
-  <meta name="og:description" content={data.description} />
-  <meta property="og:image" content={data.image?.url} />
-  <meta name="twitter:description" content={data.description} />
-  <meta name="twitter:image" content={data.image?.url} />
+  <meta name="description" content={data.collection?.description} />
+  <meta name="og:description" content={data.collection?.description} />
+  <meta property="og:image" content={data.collection?.image?.url} />
+  <meta name="twitter:description" content={data.collection?.description} />
+  <meta name="twitter:image" content={data.collection?.image?.url} />
 </svelte:head>
 
 <div class="order-last min-h-screen w-full md:order-none">
   <ul
     class="grid grid-flow-row grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3"
   >
-    {#each data.products?.edges || [] as product, index (product.node.handle)}
+    {#each data.collection?.products.edges || [] as product, index (product.node.handle)}
       <li class="aspect-square w-full">
         <Card
           productProps={product.node}
