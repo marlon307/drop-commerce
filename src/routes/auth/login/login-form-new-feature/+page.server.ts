298d944fe532747e061016927a7a4043253a1eac
marlon307 <marlon-ramosb@hotmail.com>
2025-10-05 19:15:42 -0300

    up

---
+++ src/routes/auth/login/login-form-new-feature/+page.server.ts
@@ -0,0 +1,45 @@
+/** @type {import('./$types').Actions} */
+
+import { fail, redirect } from '@sveltejs/kit';
+import { z } from "zod";
+import { accessTokenCustomerCreate } from "$lib/shopify";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const actions = {
+  login: async ({ request, cookies }) => {
+    let data;
+    try {
+      const formData = await request.formData()
+      data = schema.parse({
+        email: formData.get('email'),
+        password: formData.get('password'),
+      });
+    } catch (error) {
+      return fail(400, { status: 400, message: 'Verifique se todos os campos estão preenchidos.', fields: true });
+    }
+
+    const token = await accessTokenCustomerCreate({
+      email: data.email,
+      password: data.password,
+    });
+
+    if (token.customerUserErrors.length) {
+      return fail(400, { status: 400, message: token.customerUserErrors.map((err) => err.message), notUserExist: true });
+    }
+
+    cookies.set('sessionid', token.customerAccessToken.accessToken, {
+      path: '/',
+      expires: new Date(token.customerAccessToken.expiresAt),
+      priority: 'high',
+      httpOnly: true,
+    });
+
+    throw redirect(303, '/conta');
+  }
+};
+
+
---
+++ src/routes/auth/login/login-form-new-feature/data.remote.ts
@@ -0,0 +1,29 @@
+import { form, getRequestEvent } from "$app/server";
+import { accessTokenCustomerCreate } from "$lib/shopify";
+import { fail, redirect } from "@sveltejs/kit";
+import z from "zod";
+
+export const loginUser = form(
+  z.object({
+    email: z.email(),
+    password: z.string().min(6, "Minimo 6 caracteres"),
+  }),
+  async ({ email, password }) => {
+    const token = await accessTokenCustomerCreate({
+      email,
+      password,
+    });
+
+    if (token.customerUserErrors.length) {
+      return fail(400, { status: 400, message: token.customerUserErrors.map((err) => err.message), notUserExist: true });
+    }
+    const { cookies } = getRequestEvent();
+    cookies.set('sessionid', token.customerAccessToken.accessToken, {
+      path: '/',
+      expires: new Date(token.customerAccessToken.expiresAt),
+      priority: 'high',
+      httpOnly: true,
+    });
+
+    throw redirect(303, '/conta');
+  });
